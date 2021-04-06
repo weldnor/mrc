291f40143cc1b956a290529efef07f4a7595fe87
weldnor <kolesov.anton.s@gmail.com>
2021-04-07 01:28:46 +0300

    Disable spring test

--- backend/src/test/java/me/weldnor/mrc/AppTests.java
+++ backend/src/test/java/me/weldnor/mrc/AppTests.java
@@ -1,13 +1,5 @@
 package me.weldnor.mrc;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
 class AppTests {
-
-    @Test
-    void contextLoads() {
-    }
 
 }
