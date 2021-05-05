edc38d3cd1ef1ae45c612be97208486c0f98cdb3
weldnor <kolesov.anton.s@gmail.com>
2021-05-05 03:58:53 +0300

    Add ExceptionHandlerControllerAdvice

---
+++ backend/src/main/java/me/weldnor/mrc/advice/ExceptionHandlerControllerAdvice.java
@@ -0,0 +1,35 @@
+package me.weldnor.mrc.advice;
+
+
+import me.weldnor.mrc.exception.ApiError;
+import me.weldnor.mrc.exception.globalrole.GlobalRoleNotFoundException;
+import me.weldnor.mrc.exception.room.RoomNotFoundException;
+import me.weldnor.mrc.exception.user.UserNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ControllerAdvice
+public class ExceptionHandlerControllerAdvice {
+    @ExceptionHandler({
+            GlobalRoleNotFoundException.class,
+            UserNotFoundException.class,
+            RoomNotFoundException.class,
+    })
+    @ResponseStatus(value = HttpStatus.NOT_FOUND)
+    public @ResponseBody
+    ApiError handleResourceNotFoundExceptions(final Exception exception) {
+        return new ApiError(exception);
+    }
+
+    @ExceptionHandler(Exception.class)
+    @ResponseStatus(value = HttpStatus.INTERNAL_SERVER_ERROR)
+    public @ResponseBody
+    ApiError handleException(final Exception exception) {
+        exception.printStackTrace();
+        return new ApiError(exception);
+    }
+
+}
