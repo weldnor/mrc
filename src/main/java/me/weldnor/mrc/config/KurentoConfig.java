50ffd0496fac443229f107c3154f49c39046198c
weldnor <kolesov.anton.s@gmail.com>
2021-05-13 22:10:56 +0300

    Add kurento property, system.properties file

--- src/main/java/me/weldnor/mrc/config/KurentoConfig.java
+++ src/main/java/me/weldnor/mrc/config/KurentoConfig.java
@@ -1,13 +1,18 @@
 package me.weldnor.mrc.config;
 
 import org.kurento.client.KurentoClient;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class KurentoConfig {
+
+    @Value("${endpoints.kurento}")
+    private String kurentoWsUrl;
+
     @Bean
     public KurentoClient kurentoClient() {
-        return KurentoClient.create();
+        return KurentoClient.create(kurentoWsUrl);
     }
 }
